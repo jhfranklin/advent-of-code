17e8fce78b6d24b6ce943a518f4783eea6c06fc8
Jack Franklin <jackhenryfranklin@gmail.com>
2021-11-17 22:39:44 +0000

    10p2 in progress - need to find next angle

--- src/2019/day10/day10.jl
+++ src/2019/day10/day10.jl
@@ -27,6 +27,8 @@ end
 
 getasteroidlocations(bit_map) = Set([Asteroid(idx[2]-1, idx[1]-1) for idx ∈ CartesianIndices(bit_map) if bit_map[idx]])
 
+distance(asteroid) = sqrt(asteroid.x^2 + asteroid.y^2)
+
 function converttoangle(base,asteroid_to_test)
     sight_vector = asteroid_to_test - base
     return Angle(sight_vector.x // sight_vector.y, sight_vector.y<0)
@@ -46,5 +48,36 @@ function part1()
     return maximum(values(num_detected))
 end
 
+function part2()
+    asteroid_map = getinput() |> parseinput
+    asteroids = getasteroidlocations(asteroid_map)
+    num_detected = Dict{Asteroid,Int}()
+    for base ∈ asteroids
+        sight_angles = Set{Angle}()
+        for tester ∈ setdiff(asteroids, [base])
+            converttoangle(base, tester)
+            push!(sight_angles, converttoangle(base, tester))
+        end
+        num_detected[base] = length(sight_angles)
+    end
+    station = findmax(num_detected)[2]
+    all_angles = Set([converttoangle(station, ast) for ast ∈ asteroids if ast != station])
+    current_angle = Angle(-1//0,false)
+    num_asteroids_destroyed = 0
+    current = station
+    while num_asteroids_destroyed < 200
+        asteroids_on_angle = [ast for ast ∈ asteroids if converttoangle(station,ast) == current_angle]
+        next_angle
+        if length(asteroids_on_angle) == 0
+            continue
+        end
+        nearest_asteroid = asteroids_on_angle[findmin(distance.(asteroids_on_angle))[2]]
+        asteroids = setdiff(asteroids, [nearest_asteroid])
+        #next_angle
+        num_asteroids_destroyed += 1
+    end
+    return nearest_asteroid.x * 100 + nearest.asteroid.y
+end
+
 println("part 1: ", part1())
-#println("part 2: ", part2())
+println("part 2: ", part2())
