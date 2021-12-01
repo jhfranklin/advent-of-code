753a43aae01e4ed444bb532c33fe2ef761249f97
Jack Franklin <jackhenryfranklin@gmail.com>
2021-12-01 07:45:48 +0000

    2021 day 1

---
+++ src/2021/day01/day01.jl
@@ -0,0 +1,17 @@
+using AdventOfCode
+
+getinput() = readlines(getinputpath(2021, 1)) .|> x->parse(Int,x)
+
+function part1()
+    input = getinput()
+    return sum(diff(input) .> 0)
+end
+
+function part2()
+    input = getinput()
+    roll = [input[i]+input[i+1]+input[i+2] for i in 1:length(input)-2]
+    return sum(diff(roll) .> 0)
+end
+
+println("part 1: ", part1())
+println("part 1: ", part2())
