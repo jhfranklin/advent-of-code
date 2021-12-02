3e64abac2117d0971b93ac93bde1b7f8bdc1025e
Jack Franklin <jackhenryfranklin@gmail.com>
2021-12-02 07:44:27 +0000

    Add function to download data from AoC

--- src/AdventOfCode.jl
+++ src/AdventOfCode.jl
@@ -1,9 +1,21 @@
 module AdventOfCode
 
 export getinputpath
+export getdata
 
 function getinputpath(year,day,sample=false)
     return joinpath(@__DIR__, string(year), "day" * lpad(day,2,"0"), "day" * lpad(day,2,"0") * (sample ? ".sample" : ".in"))
+end
+
+using HTTP
+
+function getdata(year,day)
+    path = joinpath(@__DIR__, string(year), "day" * lpad(day,2,"0"), "day" * lpad(day,2,"0") * ".in")
+    session = joinpath(@__DIR__, "session_id.txt") |> readline
+    data = HTTP.get("https://adventofcode.com/" * string(year) * "/day/" * string(day) * "/input"; cookies = Dict("session" => session)).body |> String
+    f = open(path, "w")
+    write(f, data)
+    close(f)
 end
 
 end # module
